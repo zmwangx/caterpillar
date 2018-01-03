5edb1fbd23de47a9c70615e58d8eb65ab1c6cf85
Zhiming Wang <zmwangx@gmail.com>
2018-01-03 10:01:43 -0500

    caterpillar.merge: apply aac_adtstoasc bsf in final concat phase

--- src/caterpillar/merge.py
+++ src/caterpillar/merge.py
@@ -174,11 +174,13 @@ def incremental_merge(m3u8_file: pathlib.Path, output: pathlib.Path,
 
             command = ['ffmpeg', '-hide_banner', '-loglevel', 'info',
                        '-f', 'concat', '-i', 'concat.txt',
-                       '-c', 'copy', '-movflags', 'faststart', '-y', output.as_posix()]
+                       '-c', 'copy', '-bsf:a', 'aac_adtstoasc', '-movflags', 'faststart',
+                       '-y', output.as_posix()]
         elif concat_method == 'concat_protocol':
             ffmpeg_input = 'concat:' + '|'.join(f'{i}.ts' for i in range(1, playlist_index + 1))
             command = ['ffmpeg', '-hide_banner', '-loglevel', 'info', '-i', ffmpeg_input,
-                       '-c', 'copy', '-movflags', 'faststart', '-y', output.as_posix()]
+                       '-c', 'copy', '-bsf:a', 'aac_adtstoasc', '-movflags', 'faststart',
+                       '-y', output.as_posix()]
         else:
             raise NotImplementedError(f"unrecognized concat method '{concat_method}'")
 
